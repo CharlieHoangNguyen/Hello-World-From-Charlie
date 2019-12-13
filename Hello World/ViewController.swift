17041ab587ab6818e0462d5464b37a366cce98f3
Charlie Nguyen <charliehoangnguyen@gmail.com>
2019-12-13 11:24:47 -0800

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Charlie on 12/13/19.
+//  Copyright © 2019 Charlie Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
